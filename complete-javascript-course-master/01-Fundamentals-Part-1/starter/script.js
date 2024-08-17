37a4b6601d6ab118cc3b175ed369e44b3ba09eae
strict-mode <178635456+strict-mode@users.noreply.github.com>
2024-08-17 20:21:03 +0900

    two value, operators, string practice

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,27 @@
+console.log("hello");
+let firstName = "Jonas";
+
+firstName = "Jake";
+
+console.log(firstName);
+
+// Two values
+// 1. Object 2. Primitives
+
+//const ageJonas = 2037-1991;
+//const ageSarah = 2037-1991
+//console.log(ageJonas,ageSarah);
+
+const str = "hello";
+const str2 = "there";
+
+console.log(str + " " + str2);
+console.log(`${str} ${str2}`);
+
+let x = 10 + 5;
+
+console.log((x += 10)); //x=x+10
+console.log((x *= 4)); // x=x*4
+console.log(++x);
+
+console.log(1 > 2); // false
